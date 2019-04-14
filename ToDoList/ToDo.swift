40abcda2f7777f0b8a621df9f38d6a92c843c3be
student15 <student15@student15s-macbook-air.local>
2019-04-14 17:40:52 -0500

    First part with just configuring the Table View Controller

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,19 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by student15 on 4/14/19.
+//  Copyright © 2019 student15. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo {
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+}
+
+
---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by student15 on 4/14/19.
+//  Copyright © 2019 student15. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var todos = [ToDo]()
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
+    }
+    
+    override func tableView(_ table: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier") else {
+            fatalError("Could not dequeue a cell")
+        }
+        let todo = todos[indexPath.row]
+        cell.textLabel?.text = todo.title
+        return cell
+    }
+    
+}
+
